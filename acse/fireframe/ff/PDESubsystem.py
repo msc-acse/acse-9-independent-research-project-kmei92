3f09adce8cec3eadc8326e315f3158af45ab8784
Keer Mei <keermei1992@gmail.com>
2019-08-01 11:22:39 +0100

    precommit before overhaul of PDEsystem solve

--- acse/fireframe/ff/PDESubsystem.py
+++ acse/fireframe/ff/PDESubsystem.py
@@ -1,6 +1,6 @@
 import firedrake as fd
 
-class PDESubsystembase:
+class PDESubsystem:
 	"""
 	This class was adapted from Mikael Mortensen on July, 2019.
 	Source code can be found here:
@@ -15,7 +15,9 @@ class PDESubsystembase:
 		self.prm = solver_namespace['prm']
 		self.name = name
 		self.constants = constants
-		self.bcs = bcs
+		# print('subsystem_bcs', bcs)
+		self.system_bcs = bcs
+		# print('system_bcs', self.system_bcs)
 		self.setup_base()
 
 	def setup_base(self):
@@ -36,6 +38,13 @@ class PDESubsystembase:
 		"""
 		form_args = self.solver_namespace['form_args']
 		self.form_args = form_args
+
+		#boundary set up
+		self.bcs = {}
+		for var, boundaries in self.system_bcs.items():
+			self.bcs.update([(var, boundaries[0])])
+		# print('bcs', self.bcs)
+
 
 	def get_form(self, form_args, constants):
 		"""
@@ -62,16 +71,30 @@ class PDESubsystembase:
 
 	def setup_prob(self):
 		self.problems = [] # create a list
+		bounds = self.bcs # create a copy of the boundary conditions
+
 		i = 1 # need this index to create corresponding problems
 		for name in self.vars:
-			if self.prm['order'][name] > 1: # for nonlinear problems
-				print(name+'_')
-				self.problems.append(fd.NonlinearVariationalProblem(self.F[i], self.form_args[name+'_'], bcs=self.bcs[i-1]))
-				i += 1
+			if bounds[name]: # checks if there are boundary conditions associated with this variable
+				if self.prm['order'][name] > 1: # for nonlinear problems
+					# print(name+'_')
+					self.problems.append(fd.NonlinearVariationalProblem(self.F[i], self.form_args[name+'_'], bcs=bounds[name][0]))
+					i += 1
+					# bounds[name].pop(0)
+				else:
+					# print('setting up linear problem')
+					self.problems.append(fd.LinearVariationalProblem(self.a[i], self.L[i], self.form_args[name+'_'], bcs=bounds[name][0]))
+					i += 1
+					# bounds[name].pop(0)
 			else:
-				print('setting up linear problem')
-				self.problems.append(fd.LinearVariationalProblem(self.a[i], self.L[i], self.form_args[name+'_'], bcs=self.bcs[i-1]))
-				i += 1
+				if self.prm['order'][name] > 1: # for nonlinear problems
+					# print(name+'_')
+					self.problems.append(fd.NonlinearVariationalProblem(self.F[i], self.form_args[name+'_']))
+					i += 1
+				else:
+					# print('setting up linear problem')
+					self.problems.append(fd.LinearVariationalProblem(self.a[i], self.L[i], self.form_args[name+'_']))
+					i += 1
 
 	def setup_solver(self):
 		"""
@@ -84,24 +107,30 @@ class PDESubsystembase:
 		self.solvers = [] # create a list
 		i = 0 # need this index to create corresponding
 		for name in self.vars:
-			print(i)
+			# print(i)
 			if self.prm['order'][name] > 1:
 				self.solvers.append(fd.NonlinearVariationalSolver(self.problems[i]))
 				i += 1
-			else:
-				self.solvers.append(fd.LinearVariationalSolver(self.problems[i], solver_parameters={'ksp_type': self.prm['linear_solver'][name],
-																	   'pc_type': self.prm['precond'][name]}))
+			elif name in self.prm['ksp_type'] and name not in self.prm['precond']: # if only iterative methods are specified
+				self.solvers.append(fd.LinearVariationalSolver(self.problems[i], solver_parameters={'ksp_type': self.prm['linear_solver'][name]}))
 				i += 1
+			elif name not in self.prm['ksp_type'] and name in self.prm['precond']: # if only preconditioners are specified
+				self.solvers.append(fd.LinearVariationalSolver(self.problems[i], solver_parameters={'pc_type': self.prm['precond'][name]}))
+				i += 1
+			else: # use default settings
+				self.solvers.append(fd.LinearVariationalSolver(self.problems[i]))
 
-class PDESubsystem(PDESubsystembase):
-	"""
-
-	This class was adapted from Mikael Mortensen on July, 2019.
-	Source code can be found here:
-	https://bitbucket.org/simula_cbc/cbcpdesys/src/master/cbc/pdesys/PDESubSystems.py
-
-	Description:
-	Base class for most PDESubSystems
-	"""
-	def __init__(self, solver_namespace, var_sequence, name, constants, bcs):
-			PDESubsystembase.__init__(self, solver_namespace, var_sequence, name, constants, bcs)
+# """
+# This class is no longer used
+# class PDESubsystem(PDESubsystembase):
+#
+# 	This class was adapted from Mikael Mortensen on July, 2019.
+# 	Source code can be found here:
+# 	https://bitbucket.org/simula_cbc/cbcpdesys/src/master/cbc/pdesys/PDESubSystems.py
+#
+# 	Description:
+# 	Base class for most PDESubSystems
+#
+# 	def __init__(self, solver_namespace, var_sequence, name, constants, bcs):
+# 			PDESubsystembase.__init__(self, solver_namespace, var_sequence, name, constants, bcs)
+# """
--- acse/fireframe/ff/PDESystem.py
+++ acse/fireframe/ff/PDESystem.py
@@ -13,13 +13,16 @@ https://bitbucket.org/simula_cbc/cbcpdesys/src/master/cbc/pdesys/PDESystem.py
 default_solver_parameters = {
 	'degree': defaultdict(lambda: 1), # default function space dimension
 	'family': defaultdict(lambda: 'CG'), # default trial/test functions
-	'linear_solver': defaultdict(lambda: ''),
+	'ksp_type': defaultdict(lambda: ''),
+	'precond': defaultdict(lambda: ''),
 	'order' : defaultdict(lambda: 1), # default order of function spaces
 	'space' : defaultdict(lambda: fd.FunctionSpace), # default functionspace
 	'subsystem_class': defaultdict(lambda: None), # Users MUST SPECIFY what class object (ex. navier stokes, reactions) to use for each subsystem
 	'T': 10., # End time for simulation,
 	'dt': 0.001  # timestep,
 }
+solver_parameters  = copy.deepcopy(default_solver_parameters)
+
 
 
 def recursive_update(dst, src):
@@ -63,13 +66,17 @@ class PDESystem:
 		self.mesh = mesh
 		self.prm = parameters
 		self.subsystem = self.prm['subsystem_class']
+		self.constants = {}
 
-		self.t0 = 0                                           # start time
-		self.tend = self.prm['T']                                         # simulation end time
-		self.dt = self.prm['dt']                                           # time step
+		self.tstart = 0                                           			# start time
+		self.t = fd.Constant(self.tstart)									# create a time variable
+		self.tend = self.prm['T']                                         	# simulation end time
+		self.dt = self.prm['dt']                                           	# time step
 
 		self.pdesubsystems = dict((name, None) for name in self.system_names)
-		self.bc = dict((name,   []) for name in self.system_names)
+		self.bc = {}
+		self.bc_expr = [[None, None]]
+		self.var_seq = []
 
 		self.setup_system()
 
@@ -90,6 +97,7 @@ class PDESystem:
 			for n in sub_system:       # Run over all individual components
 				self.names.append(n)
 
+		self.bc.update(dict((name,   dict((name, [[None], [None]]) for name in self.names)) for name in self.system_names)) # setup boundary condition lists for each subsystem
 		self.define_function_spaces(self.mesh, self.prm['degree'], self.prm['space'],
 		self.prm['order'], self.prm['family']) # removed cons
 		self.setup_subsystems(self.prm['order'])
@@ -153,7 +161,7 @@ class PDESystem:
 		variational form. It is dependent on the variable names given to the PDESystem
 		"""
 		V, sys_comp = self.V, self.system_composition
-		form_args = {}
+		form_args = {'t': self.t} # create the time variable first
 		for sub_sys in sys_comp:
 			for name in sub_sys:
 				if order[name] > 1:
@@ -170,7 +178,7 @@ class PDESystem:
 
 		self.form_args = form_args
 
-	def solve(self):
+	def solve(self, time_update=False):
 		"""
 		This function was adapted from Mikael Mortensen on July, 2019.
 		Source code can be found here:
@@ -179,31 +187,85 @@ class PDESystem:
 		Description:
 		This function calls on each of the available pdesubsystems and solve for their respective variables
 		"""
+		forms = []
 		solvers = []
-		sub_vars = []
+		''''sub_vars = []''''
 		for name in self.system_names:
-			for solver in self.pdesubsystems[name].solvers:
-				solvers.append(solver)
-			for var in self.pdesubsystems[name].vars:
-				sub_vars.append(var)
+			'''for solver in self.pdesubsystems[name].solvers:
+				solvers.append(solver)'''
+			'''for var in self.pdesubsystems[name].vars:
+				sub_vars.append(var)'''
+			for key, form in self.pdesubsystems[name].F.items():
+				forms.append(form)
 
-		print('solvers: ', solvers)
-		print('sub_vars: ', sub_vars)
+		boundaries = [None] * len(self.var_seq) # create how many boundaries
+		set1 = set(self.var_seq)
+		abacus = dict.fromkeys(set1, 0)
+		counter = 0
 
-		t0 = self.t0
+		for var in self.var_seq:
+			for system in self.bc:
+				if var in abacus and len(self.bc[system][var][0]) > abacus[var]:
+					abacus[var] += 1
+					boundaries[counter] = self.bc[syste][var][0][abacus[var]]
+				counter += 1
+		# print('solvers: ', solvers)
+		# print('sub_vars: ', sub_vars)
+
+		tstart = self.tstart
 		tend = self.tend
 		dt = self.dt
 		print('solving from master PDESystem')
-		while t0 < tend:
-			for i in range(len(solvers)):
-				solvers[i].solve()
+		print(self.bc['T']['T'])
 
-			for var in sub_vars:
-				self.form_args[var+'_n'].assign(self.form_args[var+'_'])
+		if time_update:
+			while tstart < tend:
+				# solve current timestep
+				'''for i in range(len(solvers)):
+					solvers[i].solve()'''
+				# solve current timestep variables
+				for i, var in enumerate(self.var_seq):
+					fd.solve(forms[i] == 0, self.form_args[var+'_'], bcs=boundaries[i])
+					if i < len(self.bc_expr):
+						print('updated boundaries for %s' %var)
+						# update boundary conditions
+						boundaries[i] = [fd.DirichletBC(self.V[var], self.bc_expr[i][0], self.bc_expr[i][1])]
+					'''counter = 0
+					for system in self.bc:
+						# print(counter)
+						# print(self.bc_expr[0][1])
+						# print(self.bc[system][var][1])
+						print(self.bc[system][var][1])
+						if var in self.bc[system] and self.bc[system][var][1][counter] == 'update':
+							# print('updating %s' % var)
+							self.bc[system][var][0] = [fd.DirichletBC(self.V[var], self.bc_expr[counter][0], self.bc_expr[counter][1])]
+							counter += 1'''
+				# assign next timestep variables
+				for var in self.var_seq:
+					self.form_args[var+'_n'].assign(self.form_args[var+'_'])
+				# time step
+				tstart += dt
+				self.t.assign(tstart)
 
-			t0 += dt
-			if( np.abs( t0 - np.round(t0,decimals=0) ) < 1.e-8):
-				print('time = {0:.3f}'.format(t0))
+				#print progress
+				if( np.abs( tstart - np.round(tstart,decimals=0) ) < 1.e-8):
+					print('time = {0:.3f}'.format(tstart))
+		else:
+			while tstart < tend:
+				# solve current timestep
+				for i in range(len(solvers)):
+					solvers[i].solve()
+				# assign next timestep variables
+				for var in sub_vars:
+					self.form_args[var+'_n'].assign(self.form_args[var+'_'])
+
+
+				# time step
+				tstart += dt
+
+				#print progress
+				if( np.abs( tstart - np.round(tstart,decimals=0) ) < 1.e-8):
+					print('time = {0:.3f}'.format(tstart))
 
 	def add_subsystem(self, composition, parameters):
 		"""
@@ -226,4 +288,43 @@ class PDESystem:
 		self.prm = recursive_update(self.prm, parameters)
 		self.system_names = []
 		self.names = []
-		self.setup_system()
+
+		for sub_system in composition:
+			system_name = ''.join(sub_system)
+			self.bc.update(dict([(system_name,   dict((name, [[None], [None]]) for name in composition))])) # setup boundary condition lists for each subsystem
+			self.system_names.extend(system_name)
+			for n in sub_system:       # Run over all individual components
+				self.names.extend(n)
+
+		self.bc.update(dict((name,   dict((name, [[None], [None]]) for name in self.names)) for name in self.system_names)) # setup boundary condition lists for each subsystem
+		self.define_function_spaces(self.mesh, self.prm['degree'], self.prm['space'],
+		self.prm['order'], self.prm['family']) # removed cons
+		self.setup_subsystems(self.prm['order'])
+		self.setup_form_args(self.prm['order'])
+
+	def setup_initial(self, var, expression, mixedspace=False, **kwargs):
+		"""
+		**kwargs
+		index: the specific subspace of the MixedFunctionSpace to apply the initial condition
+		"""
+		if not mixedspace:
+			self.form_args[var].interpolate(expression)
+		else:
+			split = self.form_args[var].split()
+			split[kwargs['index']].interpolate(expression)
+
+	def setup_constants(self, dictionary):
+		"""
+		params: dictionary - keys with the same name as the arguments used in the variational forms,
+		values can be firedrake Constants, Conditionals, or other firedrake classes (ex. FacetNormal)
+		"""
+		self.constants.update(dictionary)
+
+	def define(self, var_seq, name):
+		self.var_seq.extend(var_seq)
+		# try:
+		# 	self.pdesubsystems[name] = self.subsystem[name](vars(self), var_seq, name, self.constants, self.bc[name]) # index 0 for the list of boundary conditions
+		# except:
+		# 	print("self.subsystem[%s] is not defined" % (name))
+		# print(self.bc[name])
+		self.pdesubsystems[name] = self.subsystem[name](vars(self), var_seq, name, self.constants, self.bc[name]) # index 0 for the list of boundary conditions
