8773767740cfd126839c9938f0c8ef47cdf91bfe
Keer Mei <keermei1992@gmail.com>
2019-08-12 21:34:28 +0100

    changed test location

---
+++ test/tests.py
@@ -0,0 +1,22 @@
+import sys
+sys.path.append("..")
+from PDESystem import *
+from PDESubsystem import *
+from demo.pdeforms import navier_stokes
+import firedrake as fd
+
+def test_load_parameters():
+
+    diction = {
+	'space': {'u': fd.VectorFunctionSpace},
+	'degree': {'u': 2},
+	'ksp_type': {'u': 'gmres', 'p' :'gmres'},
+	'subsystem_class' : {'up' : navier_stokes},
+	'precond': {'u': 'sor', 'p': 'sor'},
+	'dt' : 0.01,
+	'T' : 10
+	}
+
+    solver_parameters = recursive_update(default_solver_parameters, diction)
+
+    assert(False is True)
